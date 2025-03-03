06c53db99912660e97a1f5275afb69dc2df5db1f
Nazar263 <marcaknazar370@gmail.com>
2025-03-03 21:51:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function plus() {
+let input=
+document.getElementById("input_text1");
+input.value = Number(input.value)+1;
+}
+function minus() {
+let input=
+document.getElementById("input_text1");
+input.value = Number(input.value)-1;
+}
+function clean() {
+  document.getElementById("input_text1").value ="0";
+}
